1cad6721286f0b3a39c33398545483aee1d9947c
Hasse Ramlev Hansen <hasse@ramlev.dk>
2014-06-04 17:06:03 +0200

    Add POST authenticate user API Callback

--- src/Harvester/APIBundle/Controller/UserController.php
+++ src/Harvester/APIBundle/Controller/UserController.php
@@ -12,7 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends FOSRestController  implements ClassResourceInterface
+class UserController extends FOSRestController implements ClassResourceInterface
 {
     /**
      * @ApiDoc(
@@ -25,7 +25,7 @@ class UserController extends FOSRestController  implements ClassResourceInterfac
      *   requirements={
      *     {
      *       "name": "_format",
-     *       "dataTYpe": "integer",
+     *       "dataType": "integer",
      *       "requirement": "json|xml",
      *     }
      *   }
@@ -43,6 +43,54 @@ class UserController extends FOSRestController  implements ClassResourceInterfac
     /**
      * @ApiDoc(
      *   section="Users",
+     *   description="Autheticate user",
+     *   statusCodes={
+     *     200="Returned when authenticated",
+     *     401="Returned when authentication failed",
+     *   },
+     *   requirements={
+     *     {
+     *       "name": "email",
+     *       "dataType": "string",
+     *       "requirement": "valid email",
+     *       "description": "The email address"
+     *     },
+     *     {
+     *       "name": "password",
+     *       "dataType": "string",
+     *       "requirement": "Hashed password",
+     *       "description": "Password"
+     *     },
+     *     {
+     *       "name": "_format",
+     *       "dataType": "integer",
+     *       "requirement": "json|xml",
+     *     }
+     *   }
+     * )
+     */
+    public function postLoginAction()
+    {
+        $repository = $this->getDoctrine()
+            ->getRepository('HarvesterFetchBundle:User');
+
+        $query = $repository->createQueryBuilder('u')
+            ->where('u.email = :email')
+            ->andWhere('u.password = :password')
+            ->setParameter('email', $this->get('request')->request->get('email'))
+            ->setParameter('password', $this->get('request')->request->get('password'))
+            ->getQuery();
+
+        $user = $query->getResult();
+
+        $view = $this->view($user, $user ? 200 : 401);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @ApiDoc(
+     *   section="Users",
      *   description="Returns a User",
      *   statusCodes={
      *     200="Returned when successful",
@@ -51,13 +99,13 @@ class UserController extends FOSRestController  implements ClassResourceInterfac
      *   requirements={
      *     {
      *       "name": "user_id",
-     *       "dataTYpe": "integer",
+     *       "dataType": "integer",
      *       "requirement": "\d+",
      *       "description": "The Harvest User Id"
      *     },
      *     {
      *       "name": "_format",
-     *       "dataTYpe": "integer",
+     *       "dataType": "integer",
      *       "requirement": "json|xml",
      *     }
      *   }
@@ -89,7 +137,7 @@ class UserController extends FOSRestController  implements ClassResourceInterfac
      *     },
      *     {
      *       "name": "_format",
-     *       "dataTYpe": "integer",
+     *       "dataType": "integer",
      *       "requirement": "json|xml",
      *     }
      *   }
