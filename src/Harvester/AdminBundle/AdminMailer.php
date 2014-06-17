aad7890b650d026dc04b78ba851647ee844c39dd
Hasse Ramlev Hansen <hasse@ramlev.dk>
2014-06-17 11:06:20 +0200

    Commented the code

--- src/Harvester/AdminBundle/AdminMailer.php
+++ src/Harvester/AdminBundle/AdminMailer.php
@@ -8,15 +8,35 @@ use Symfony\Component\Templating\EngineInterface;
 
 class AdminMailer
 {
+    /**
+     * @var \Swift_Mailer
+     */
     protected $mailer;
+    /**
+     * @var \Symfony\Component\Templating\EngineInterface
+     */
     protected $templating;
 
+    /**
+     * @param Swift_Mailer $mailer
+     * @param EngineInterface $templating
+     */
     public function __construct(Swift_Mailer $mailer, EngineInterface $templating)
     {
         $this->mailer = $mailer;
         $this->templating = $templating;
     }
 
+    /**
+     * Send an email.
+     *
+     * @param string $from
+     * @param string $to
+     * @param string $name
+     * @param string $password
+     * @param string $subject
+     * @return int
+     */
     public function sendMail($from, $to, $name, $password, $subject)
     {
         $message = Swift_Message::newInstance()
