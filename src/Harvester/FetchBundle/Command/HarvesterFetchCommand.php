c52c9271d08b6cb62e8b7d4be6e1865d7d7b9469
André Kallehauge <andrekallehauge@gmail.com>
2014-07-16 11:05:31 +0200

    Added support for "Updated since xxx" options in the "harvester:fetch" command.

--- src/Harvester/FetchBundle/Command/HarvesterFetchCommand.php
+++ src/Harvester/FetchBundle/Command/HarvesterFetchCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use DateTime;
+use DateInterval;
 use Harvest_Range;
 use Harvest_Exception;
 
@@ -40,13 +41,37 @@ class HarvesterFetchCommand extends ContainerAwareCommand
                 'preserve-roles',
                 null,
                 InputOption::VALUE_NONE,
-                'If set, preserve the admin roles set on the user, add role to new users'
+                'If set, preserve the admin roles set on the user, add role to new users')
+            ->addOption(
+                'updated-yesterday',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, get the entries that have been updated since yesterday')
+            ->addOption(
+                'updated-week',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, get the entries that have been updated within the last 7 days')
+            ->addOption(
+                'updated-month',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, get the entries that have been updated within the last 30 days')
+            ->addOption(
+                'updated-year',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, get the entries that have been updated this year'
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Get the "real" reports HarvestAPI.
         $api = $this->getContainer()->get('harvest_app_reports')->getApi();
+        // Get our own, extended API, that only supports "API", not "Reports".
+        $extended_api = $this->getContainer()->get('harvest_adapter')->getApi();
+        // Get doctrine.
         $doctrine = $this->getContainer()->get('doctrine');
 
         // Check if we have a valid connection to the API.
@@ -61,17 +86,52 @@ class HarvesterFetchCommand extends ContainerAwareCommand
         $from_date = $input->getArgument('from-date');
         $to_date = $input->getArgument('to-date');
 
+        // Get the current date.
+        $date_today = new DateTime('now');
+
+        // Updated: Yesterday.
+        if ($input->getOption('updated-yesterday')) {
+            // Set the period to days and deduced "1" from the current date;
+            $interval = new DateInterval('P1D');
+            $updated_since = $date_today->sub($interval)->format('Y-m-d');
+        }
+        // Updated: within 7 days.
+        if ($input->getOption('updated-week')) {
+            // Set the period to days and deduced "7" from the current date;
+            $interval = new DateInterval('P7D');
+            $updated_since = $date_today->sub($interval)->format('Y-m-d');
+        }
+        // Updated: within 30 days.
+        if ($input->getOption('updated-month')) {
+          // Set the period to days and deduced "30" from the current date;
+            $interval = new DateInterval('P30D');
+            $updated_since = $date_today->sub($interval)->format('Y-m-d');
+        }
+        // Updated: this year. Will go back to the first of january.
+        if ($input->getOption('updated-year')) {
+            $updated_since = $date_today->format('Y') . '-01-01';
+        }
+
+        // Set "updated since" to "null" if no argument were provided.
+        $updated_since = isset($updated_since) ? $updated_since : null;
+
         // If we have a valid callback from Harvest.
         if ($api_users->isSuccess()) {
+            // Loop through each users.
             foreach ($api_users->get('data') as $user_id => $api_user) {
+                // Output the current user to the terminal.
                 $output->writeln('<info>' . $api_user->first_name . ' ' . $api_user->last_name . '</info>');
+                $output->writeln($api_user->notes);
 
+                // Get the User Repository.
                 $doctrine->getManager()->getRepository('HarvesterFetchBundle:User')
                     ->registerUser($api_user, $input, $output);
 
-                // Fetch user entries updated within range.
+                // Set range for the Harvest data.
                 $range = new Harvest_Range($from_date, $to_date);
-                $user_entries = $api->getUserEntries($user_id, $range);
+
+                // Fetch user entries (the 3. argument is null since we don't want to use a project ID).
+                $user_entries = $extended_api->getUserEntries($user_id, $range, null, $updated_since);
 
                 // Save users entries if any is available.
                 if ($user_entries->isSuccess() && count($user_entries->get('data'))) {
