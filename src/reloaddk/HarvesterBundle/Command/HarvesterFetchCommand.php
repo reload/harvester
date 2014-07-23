3b14adf643f0696699b92e04c2a0f9d1a2434f61
Hasse Ramlev Hansen <hasse@ramlev.dk>
2014-07-23 11:04:24 +0200

    Indented files correct

--- src/reloaddk/HarvesterBundle/Command/HarvesterFetchCommand.php
+++ src/reloaddk/HarvesterBundle/Command/HarvesterFetchCommand.php
@@ -14,6 +14,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class HarvesterFetchCommand extends ContainerAwareCommand
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $date_from = new DateTime('01-01-1970');
@@ -70,12 +73,17 @@ class HarvesterFetchCommand extends ContainerAwareCommand
             );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // Get the "real" reports HarvestAPI.
         $api = $this->getContainer()->get('harvest_app_reports')->getApi();
+
         // Get our own, extended API, that only supports "API", not "Reports".
         $extended_api = $this->getContainer()->get('harvest_adapter')->getApi();
+
         // Get doctrine.
         $doctrine = $this->getContainer()->get('doctrine');
 
@@ -101,32 +109,38 @@ class HarvesterFetchCommand extends ContainerAwareCommand
                 $output->writeln('<error>The value for the "updated" argument, must be greater than 0</error>');
                 return;
             }
+
             // Set the period to days and deduced "1" from the current date;
             $interval = new DateInterval('P' . $input->getOption('updated') . 'D');
             $updated_since = $date_today->sub($interval)->format('Y-m-d');
         }
+
         // Updated: Yesterday.
         if ($input->getOption('updated-yesterday')) {
             // Set the period to days and deduced "1" from the current date;
             $interval = new DateInterval('P1D');
             $updated_since = $date_today->sub($interval)->format('Y-m-d');
         }
+
         // Updated: within 7 days.
         if ($input->getOption('updated-week')) {
             // Set the period to days and deduced "7" from the current date;
             $interval = new DateInterval('P7D');
             $updated_since = $date_today->sub($interval)->format('Y-m-d');
         }
+
         // Updated: within 30 days.
         if ($input->getOption('updated-month')) {
             // Set the period to days and deduced "30" from the current date;
             $interval = new DateInterval('P30D');
             $updated_since = $date_today->sub($interval)->format('Y-m-d');
         }
+
         // Updated: this year. Will go back to the first of january.
         if ($input->getOption('updated-year')) {
             $updated_since = $date_today->format('Y') . '-01-01';
         }
+
         // Set "updated since" to "null" if no argument were provided.
         $updated_since = isset($updated_since) ? $updated_since : null;
 
@@ -147,6 +161,7 @@ class HarvesterFetchCommand extends ContainerAwareCommand
 
                 $doctrine->getManager()->getRepository('reloaddkHarvesterBundle:User')
                     ->registerUser($api_user, $input, $output);
+
                 // Set range for the Harvest data.
                 $range = new Harvest_Range($from_date->format('Ymd'), $to_date->format('Ymd'));
 
